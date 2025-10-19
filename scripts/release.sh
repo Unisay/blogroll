af963fc5609ab316515599dc8ac63be8e649d508
Yura Lazarev <Unisay@users.noreply.github.com>
2025-10-19 18:59:43 +0200

    fix: use direct grep instead of cabal info for version extraction

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Get version from cabal file and create release tag
-VERSION=$(cabal info . | grep -E '^Version:' | awk '{print $2}')
+VERSION=$(grep "^version:" blogroll.cabal | awk '{print $2}')
 
 git tag "v$VERSION"
 git push origin "v$VERSION"
